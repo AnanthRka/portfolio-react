598303458f1b411dbf9fa61bbf79e2c62b7b836c
AnanthRka <r.ananth04@gmail.com>
2022-08-27 01:58:12 +0530

    test commit to deploy

--- react-portfolio/src/components/about/About.jsx
+++ react-portfolio/src/components/about/About.jsx
@@ -15,33 +15,33 @@ const About = () => {
           <div className="about__me-image">
               <img src={ME} alt="About Image" />
             </div>
-          </div>
+        </div>
           
-          <div className="about__content">
-            <div className="about__cards">
-              <article className='about__card'>
-                <FaAward className='about__icon'/>
-                <h5>Experience</h5>
-                <small>3+ Years Working</small>
-              </article>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
 
-              <article className='about__card'>
-                <FiUsers className='about__icon'/>
-                <h5>Clients</h5>
-                <small>200+ Worldwide</small>
-              </article>
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
 
-              <article className='about__card'>
-                <VscFolderLibrary className='about__icon'/>
-                <h5>Projects</h5>
-                <small>80+ Completed</small>
-              </article>
-            </div>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
           </div>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
       </div>
     </section>
   )
